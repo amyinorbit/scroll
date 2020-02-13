06edff1b1adf31b9ac49d6beaff12da523b0153a
Amy Parent <amy@amyparent.com>
2020-02-13 18:33:31 +0000

    Add proof-of-concept

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+    version='0.1',
+    description='Tailored, useless to anyone else BibTex/ADS bib manager',
+    url='http://github.com/amyinorbit/scroll',
+    author='Amy Parent',
+    author_email='amy@amyparent.com',
+    license='MIT',
+    packages=['scroll'],
+    zip_safe=False)
